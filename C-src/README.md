b818640c70b77c240f99611522fc03bb37072de5
Stephen M. Moraco <stephen@ironsheep.biz>
2022-06-10 19:56:11 -0600

    Completed initial release of convert utility
    - add command line parsing to convert
    - document how to use convert

--- C-src/README.md
+++ C-src/README.md
@@ -22,19 +22,10 @@ This tool is used to convert bitmaps created at the free website [Piskel](https:
 Latest Changes:
 
 ```
+10 June 2022 
+- adding missing parts (command-line options) to convert
 09 June 2022 
 - 'convert' tool is working (can still be refined)
-
-```
-
-## Known Issues
-
-Things we know about that still need attention:
-
-```
-convert needs refinement
-- add command line processing
-  - arguments output filename, image name, etc.
 ```
 
 
@@ -42,6 +33,7 @@ convert needs refinement
 
 On this Page:
 
+- Test your ability to make the convert utility
 - Create your image at website
 - Download your image as .c file
 - Build your image into convert
@@ -52,7 +44,177 @@ Additional pages:
 
 - [Top README](https://github.com/ironsheep/P2-Click-eInk) - Return to the top-level README for this repository
 
+## Test your ability to make the convert utility
 
+In the folder containing convert.c you'll fine a script named `tst`.  Run this script:
+
+```bash
+$ tst<ret>  # run the tst script which builds a test .spin2 file
+```
+
+The output from the tst run should look like:
+
+```bash
+$ ./tst
+rm -f *.o *~ core convert *.spin2
+gcc -I.   -c -o convert.o convert.c
+gcc -I.   -c -o argparse.o argparse.c
+gcc -o convert convert.o argparse.o -I.
++ ./convert -o isp_demo_coffee_image.spin2 -i coffee -v
+Verbose: ON
+Writing to: isp_demo_coffee_image.spin2
+Image is named: coffee
+
+Image is 32 X 49 px
+TST: Created isp_demo_coffee_image.spin2!
+```
+
+If all of this is present then you can see how to run the script:
+
+```bash
+$ ./convert -o isp_demo_coffee_image.spin2 -i coffee -v
+```
+
+The full convert command-line options are:
+
+```bash
+$ ./convert -h
+Usage: convert [options] -o {outfilename}
+
+A brief description of what the program does and how it works.
+
+    -h, --help                show this help message and exit
+
+convert options
+    -n, --nodo                emit messages only don't do work
+    -d, --debug               emit debug messages
+    -v, --verbose             emit verbose messages
+    -o, --outfile=<str>       filename of output file
+    -i, --imagename=<str>     label of image in memory (def: 'img')
+
+Additional description of the program after the description of the arguments.
+```
+
+*NOTE: nodo is not yet supported!*
+
+Ok, if all of this happened you are ready to move on to the next step of creating your new image(s).  We'll use a free website to create yuor image.
+
+## Create your image at website
+
+Visit the [Piskel online editor](https://www.piskelapp.com/p/create/sprite) and create your own image.
+
+NOTE: use only the colors supported by our eInk displays:
+
+| Color Name | Hex Value |
+| --- | --- |
+| Black | #000000 |
+| White | #FFFFFF
+| Light grey | #555555
+| Dark grey | #AAAAAA
+
+## Download your image as .c file
+
+At the Piskel website after editing your image then you want to save two files. 
+
+- The .piskel file source so you can reload your image into the brower and refine it.
+- The .c source file - this you'll build into the convert tool and then generate your .spin2 image source file
+
+## Build your image into convert
+
+Copy your new .c file into your convert working directory.
+Edit convert.c doing the following:
+
+| Edits needed |
+| --- |
+| (1) Change the included .c file to your new .c filename. That is, look near line(6) `#include "New Piskel-coffee-002.c"` and change **New Piskel-coffee-002.c** to the name of your new .c file.
+| (2) Look near line(77) `int imgFrameHeight = NEW_PISKEL_FRAME_HEIGHT;` and change **NEW\_PISKEL\_FRAME_HEIGHT** to the name in your new .c file.
+| (3) Look near line(78) `int imgFrameWidth = NEW_PISKEL_FRAME_WIDTH;` and change **NEW\_PISKEL\_FRAME_WIDTH** to the name in your new .c file.
+| (4) Look near line(79) `const u_int32_t *imgDataArray = &new_piskel_data[0][0];` and change **new\_piskel_data** to the name in your new .c file.
+
+Once these edits are complete then make the new utility using:
+
+```bash
+$ make clean all   # remove prior files and compile the utility
+```
+
+Your make output should look something like this:
+
+```bash
+$ make clean all 
+rm -f *.o *~ core convert *.spin2
+gcc -I.   -c -o convert.o convert.c
+gcc -I.   -c -o argparse.o argparse.c
+gcc -o convert convert.o argparse.o -I.
+```
+
+If you instead see any errors then fix them before proceeding.
+
+## Generate your image .spin2 file
+
+Next you'll run the new utilty to generate the .spin2 file you will need to include in your project to the image can be loaded to your eInk display.
+
+You will want to specify at least two options (-i -and -o) of the following:
+
+| convert Option | Example | Description
+| --- | --- | --- |
+| -o {filename} | -o myImage.spin2 | Specify the filename to be written
+| -i {imageBaseName} | -i myImage | Specify the label name of your new image bits
+| -v | -verbose  | Enable verbose messaging output
+| -d | -debug | for extra fun turn this on to see the hex values the convert utilty generated as the command output
+
+The command line for running with the above options would look like:
+
+```bash
+$ ./convert -v -o myImage.spin2 -i myImage
+```
+
+This will generate a `myImage.spin2` file with image labels within the file similar to:
+
+```
+DAT { Overlay Bitmap Data }
+
+'  Image by Convert v1.0
+
+	myImageWidth	WORD	16
+	myImageHeight	WORD	49
+	myImageBits
+		BYTE	$77, $77, $77, $77, $77, $77, $77, $77, $77, $77, $07, $77, $77, $77, $77, $77	' row 1 of 49
+...
+...
+...
+		BYTE	$77, $77, $77, $77, $77, $77, $77, $77, $77, $77, $77, $77, $77, $77, $77, $77	' row 49 of 49
+' Converted 32 x 49 (1568) pixel values into 784 bytes
+	myImageBitsEnd
+	myImageLenBytes	WORD	myImageBitsEnd - myImageBits	' Image length in bytes
+
+
+```
+
+## Include your .spin file and pace your image on screen
+
+Now that you have your .spin2 file you can include it into your project.
+ 
+```spin2
+OBJ
+     eInkDisplay : "isp_eInk_click"
+     coffeeImage : "isp_image_coffee"	' include my bitmap data
+```
+
+Lastly you'll send this image to be put on the display with something
+ like:
+ 
+```spin2
+    pImageBits, imgWidth, imgHeight := coffeeImage.imageAddress(coffeeImage.IMAGE_Coffee)
+    eInkDisplay.placeImageAtXY(10, 10, pImageBits, imgWidth, imgHeight, 90)
+```
+
+Yes, I added a method at the top of my image file: `PUB imageAddress(eImage) : pImageBits, imgWidth, imgHeight` 
+
+*This is included in the demo file of this driver. feel free to copy it to your new image file.*
+
+Ok. that's all there is to know. Have fun using your new images on your eInk displays!
+
+-Stephen
 
 ---
 
--- C-src/convert.c
+++ C-src/convert.c
@@ -1,21 +1,83 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include "argparse.h"
 
 #include "New Piskel-coffee-002.c"
 
+typedef int bool;
 #define FALSE 0
 #define TRUE (-1)
 
-int main() {
+static const char *const usages[] = {
+    "convert [options] -o {outfilename}",
+    NULL,
+};
+
+int main(int argc, const char **argv)
+{
+    bool opt_nodo = FALSE;
+    bool opt_debug = FALSE;
+    bool opt_verbose = FALSE;
+    const char *outfile = NULL;
+    const char *imageName = NULL;
+    const char *defaultImageName = "img";
+
+    struct argparse_option options[] = {
+        OPT_HELP(),
+        OPT_GROUP("convert options"),
+        OPT_BOOLEAN('n', "nodo", &opt_nodo, "emit messages only don't do work", NULL, 0, 0),
+        OPT_BOOLEAN('d', "debug", &opt_debug, "emit debug messages", NULL, 0, 0),
+        OPT_BOOLEAN('v', "verbose", &opt_verbose, "emit verbose messages", NULL, 0, 0),
+        OPT_STRING('o', "outfile", &outfile, "filename of output file", NULL, 0, 0),
+        OPT_STRING('i', "imagename", &imageName, "label of image in memory (def: 'img')", NULL, 0, 0),
+        OPT_END(),
+    };
+
+    struct argparse argparse;
+    argparse_init(&argparse, options, usages, 0);
+    argparse_describe(&argparse, "\nA brief description of what the program does and how it works.", "\nAdditional description of the program after the description of the arguments.");
+    argc = argparse_parse(&argparse, argc, argv);
+    if (opt_nodo != FALSE)
+        printf("Nodo: ON\n");
+    if (opt_debug != FALSE)
+        printf("Debug: ON\n");
+    if (opt_verbose != FALSE)
+        printf("Verbose: ON\n");
+
+    if (outfile != NULL)
+        printf("Writing to: %s\n", outfile);
+
+    if (imageName == NULL) {
+        imageName = defaultImageName;
+    }
+    if (opt_verbose) {
+        printf("Image is named: %s\n", imageName);
+    }
+
+    if (argc != 0) {
+        printf("argc: %d\n", argc);
+        int i;
+        for (i = 0; i < argc; i++) {
+            printf("argv[%d]: %s\n", i, *(argv + i));
+        }
+    }
+
+    if (outfile == NULL) {
+        printf("\n\nconvert: ERROR missing output filename\n\n");
+        argparse_usage(&argparse);
+        return 255;
+    }
+
+
     FILE *f;
-    f = fopen("image_coffee.spin2", "w");
-    typedef int bool;
-//    #define NEW_PISKEL_FRAME_WIDTH 32
-//    #define NEW_PISKEL_FRAME_HEIGHT 64
+    f = fopen(outfile, "w");
 
-// Piskel data for "New Piskel"
+    // connection from included Piskel file
+    int imgFrameHeight = NEW_PISKEL_FRAME_HEIGHT;
+    int imgFrameWidth = NEW_PISKEL_FRAME_WIDTH;
+    const u_int32_t *imgDataArray = &new_piskel_data[0][0];
 
-// static const uint32_t new_piskel_data[1][2048] = {
     int nIdx = 0;
     long value = 0l;
     long color = 0l;
@@ -23,12 +85,12 @@ int main() {
 
     bool bFirstPass = TRUE;
     bool bFoundNonEmpty = FALSE;
-    for(int row = 0; row < NEW_PISKEL_FRAME_HEIGHT; row++) {
+    for(int row = 0; row < imgFrameHeight; row++) {
         if(bFoundNonEmpty == FALSE) {
             // presscan row. If leading row and empty don't emit!
-            for(int col = 0; col < NEW_PISKEL_FRAME_WIDTH; col += 2) {
-                int nIdx = (row * NEW_PISKEL_FRAME_WIDTH) + col;
-                int value = new_piskel_data[0][nIdx];
+            for(int col = 0; col < imgFrameWidth; col += 2) {
+                int nIdx = (row * imgFrameWidth) + col;
+                int value = imgDataArray[nIdx];
                 if(value != 0) {
                     bFoundNonEmpty = TRUE;
                     break;
@@ -40,22 +102,24 @@ int main() {
             }
         }
         if(bFirstPass) {
-            printf("\nImage is %d X %d px\n", NEW_PISKEL_FRAME_WIDTH, NEW_PISKEL_FRAME_HEIGHT - emptyLineCt);
+            if(opt_verbose) {
+                printf("\nImage is %d X %d px\n", imgFrameWidth, imgFrameHeight - emptyLineCt);
+            }
             bFirstPass = FALSE;
         }
-        for(int col = 0; col < NEW_PISKEL_FRAME_WIDTH; col++) {
-            nIdx = (row * NEW_PISKEL_FRAME_WIDTH) + col;
+        for(int col = 0; col < imgFrameWidth; col++) {
+            nIdx = (row * imgFrameWidth) + col;
             value = new_piskel_data[0][nIdx];
             color = value & 0xFFFFFF;
             if(col == 0) {
-                //printf("' values 0x%X thru 0x%X\n", nIdx, nIdx + NEW_PISKEL_FRAME_HEIGHT - 1);
+                //printf("' values 0x%X thru 0x%X\n", nIdx, nIdx + imgFrameHeight - 1);
             }
             if(value == 0) {
-                printf("*   ");
+               if(opt_debug) printf("*   ");
             }
             else {
                 // printf("$%.6lX, ", color);
-                printf("$%.2lX ", color & 0xFF);
+                if(opt_debug) printf("$%.2lX ", color & 0xFF);
             }
             //if((col + 1) % 16 == 0) {
             //    printf("\n");
@@ -64,28 +128,28 @@ int main() {
             //    printf("\n");
             //}
         }
-        printf("\n");
+        if(opt_debug) printf("\n");
     }
-    printf("\n");
+    if(opt_debug) printf("\n");
 
-    int actualLineCt = NEW_PISKEL_FRAME_HEIGHT - emptyLineCt;
+    int actualLineCt = imgFrameHeight - emptyLineCt;
 
     fprintf(f, "DAT { Overlay Bitmap Data }\n\n");
     fprintf(f, "'  Image by Convert v1.0\n\n");
-    fprintf(f, "\timgWidth\tWORD\t%d\n", NEW_PISKEL_FRAME_WIDTH / 2);
-    fprintf(f, "\timgHeight\tWORD\t%d\n", actualLineCt);
-    fprintf(f, "\timgBits\n");
+    fprintf(f, "\t%sWidth\tWORD\t%d\n", imageName, imgFrameWidth / 2);
+    fprintf(f, "\t%sHeight\tWORD\t%d\n", imageName, actualLineCt);
+    fprintf(f, "\t%sBits\n", imageName);
     int rowNbr = 1;
-    for(int row = emptyLineCt; row < NEW_PISKEL_FRAME_HEIGHT; row++) {
+    for(int row = emptyLineCt; row < imgFrameHeight; row++) {
         fprintf(f, "\t\tBYTE\t");
-        for(int col = 0; col < NEW_PISKEL_FRAME_WIDTH; col += 2) {
-            int nIdx = (row * NEW_PISKEL_FRAME_WIDTH) + col;
+        for(int col = 0; col < imgFrameWidth; col += 2) {
+            int nIdx = (row * imgFrameWidth) + col;
             int valueLt = new_piskel_data[0][nIdx];
             int valueRt = new_piskel_data[0][nIdx+1];
             valueLt = (valueLt == 0) ? 0x07 : valueLt & 0x03;
             valueRt = (valueRt == 0) ? 0x07 : valueRt & 0x03;
             int packedByte = (valueLt << 4) | valueRt;
-            if(col == (NEW_PISKEL_FRAME_WIDTH - 2)) {
+            if(col == (imgFrameWidth - 2)) {
                 fprintf(f, "$%.2X", packedByte);
             }
             else {
@@ -94,10 +158,10 @@ int main() {
         }
         fprintf(f, "\t' row %d of %d\n", rowNbr++, actualLineCt);
     }
-    int nonEmptyRows = NEW_PISKEL_FRAME_HEIGHT - emptyLineCt;
-    fprintf(f, "' Converted %d x %d (%d) pixel values into %d bytes\n", NEW_PISKEL_FRAME_WIDTH, nonEmptyRows, NEW_PISKEL_FRAME_WIDTH * nonEmptyRows, (NEW_PISKEL_FRAME_WIDTH * nonEmptyRows) / 2);
-    fprintf(f, "\timgBitsEnd\n");
-    fprintf(f, "\timgLenBytes\tWORD\timgBitsEnd - imgBits\t' Image length in bytes\n");
+    int nonEmptyRows = imgFrameHeight - emptyLineCt;
+    fprintf(f, "' Converted %d x %d (%d) pixel values into %d bytes\n", imgFrameWidth, nonEmptyRows, imgFrameWidth * nonEmptyRows, (imgFrameWidth * nonEmptyRows) / 2);
+    fprintf(f, "\t%sBitsEnd\n", imageName);
+    fprintf(f, "\t%sLenBytes\tWORD\t%sBitsEnd - %sBits\t' Image length in bytes\n", imageName, imageName, imageName);
 
     fprintf(f, "\n");
    //fprintf(f, "%s\n[ %d ]\n\n", *name, number);
